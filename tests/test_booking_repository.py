1953fa7d7d23ad4d5d3d1a8da6a0391f8de128d7
Max Joseph <maxjoseph22@gmail.com>
2024-11-07 15:54:54 +0000

    api made for confirming booking and changing availability status

--- tests/test_booking_repository.py
+++ tests/test_booking_repository.py
@@ -1,5 +1,6 @@
 from lib.booking_repository import *
 from lib.booking import *
+from lib.spaces_repository import *
 
 def test_get_all_bookings(db_connection): # See conftest.py to learn what `db_connection` is.
     db_connection.seed("seeds/users_and_spaces.sql") # Seed our database with some test data
@@ -43,3 +44,19 @@ def test_delete_deletes_booking(db_connection):
         Booking(2, '2024-11-14', '2024-11-18', 'confirmed', 80, 2, 4),
     ]
 
+def test_pending_bookings_relating_to_appropriate_user(db_connection):
+    db_connection.seed("seeds/users_and_spaces.sql")
+    booking_repository = BookingRepository(db_connection)
+    spaces_repository = SpaceRepository(db_connection)
+    booking = booking_repository.find_pending_bookings(1)
+    assert booking == [Booking(3, '2024-11-15', '2024-11-16', 'pending', 60, 3, 2)]
+
+# function will find only pending bookings relating to the host, this selection of bookings can then be
+# accessed by confirm booking route  
+
+def test_pending_bookings_relating_to_appropriate_user_2(db_connection):
+    db_connection.seed("seeds/users_and_spaces.sql")
+    booking_repository = BookingRepository(db_connection)
+    spaces_repository = SpaceRepository(db_connection)
+    booking = booking_repository.find_pending_bookings(2)
+    assert booking == []
