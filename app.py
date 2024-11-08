f5b659d653a75cd15cd0ac002913146c2ade6283
Shola Forbes <shola.forbes1@gmail.com>
2024-11-08 14:30:27 +0000

    amends to dashboard

--- app.py
+++ app.py
@@ -100,8 +100,9 @@ def get_user_dashboard(id):
     user = user_repository.find(id)
     spaces = space_repository.find_spaces_linked_to_id(id)
     bookings = booking_repository.find_spaces_linked_to_id(id)
-    # requests = booking_repository.find_user_linked_to_space(id)
-    return render_template('user.html', user=user, spaces=spaces, bookings=bookings)
+    requests = booking_repository.find_pending_request_from_guest(id)
+    print (requests)
+    return render_template('user.html', user=user, spaces=spaces, bookings=bookings, requests=requests)
 
 
 # Returns the individual add new space page  
